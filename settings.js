cb9e5c98b33723b982130e6b1f3f5be323c02ec3
TIEGOTEE7 <phashatiego123@gmail.com>
2025-08-08 18:28:45 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'DAEMON Bot',
+  author: '‎',
+  botName: "SHADOW-TEE BOT",
+  botOwner: 'TIEGO', // Your name
+  ownerNumber: '27712943636', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;
